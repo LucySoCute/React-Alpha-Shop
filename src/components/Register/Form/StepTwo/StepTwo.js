da278bce0323509ef9198a21bc93c2a03d15376e
LucySoCute <cutelucysu@gmail.com>
2022-11-14 00:30:16 +0800

    renew shipping form input

--- src/components/Register/Form/StepTwo/StepTwo.js
+++ src/components/Register/Form/StepTwo/StepTwo.js
@@ -2,12 +2,18 @@ import styles from "./StepTwo.module.scss";
 import {useState} from "react";
 
 export default function StepTwo() {
+
+  const [shippingChange, setShippingChange] = useState('on');
+  const handleChange = event => {
+    setShippingChange(event.target.value);
+  };
+
   return (
     <section className={styles.stepTwo}>
       <h3 className={styles.stepTwoTitle}>運送方式</h3>
       <div className={styles.stepTwoContainer}>
         <div className={styles.choice}>
-          <input name="shippingMethod" className={styles.choiceBox} type="radio" checked/>
+          <input name="shippingMethod" className={styles.choiceBox} type="radio" value="on" checked={shippingChange === "on"} onChange={handleChange}/>
           <div className={styles.radioInfo}>
             <div className={styles.radioInfoChoice}>
               <div className={styles.shippingChoice}>標準配送</div>
@@ -17,7 +23,7 @@ export default function StepTwo() {
           </div>
         </div>
         <div className={styles.choice}>
-          <input name="shippingMethod" className={styles.choiceBox} type="radio"/>
+          <input name="shippingMethod" className={styles.choiceBox} type="radio" value="off" checked={shippingChange === "off"} onChange={handleChange}/>
           <div className={styles.radioInfo}>
             <div className={styles.radioInfoChoice}>
               <div className={`${styles.shippingChoice} ${styles.DHL}`}>DHL 貨運</div>
